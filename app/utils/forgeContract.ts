9453f18a7383eb8abf8f053ee002f2848c3644e2
David Killen <39182959+dkillen@users.noreply.github.com>
2024-10-14 17:20:16 +1100

    Update contract ABI

--- app/utils/forgeContract.ts
+++ app/utils/forgeContract.ts
@@ -3,33 +3,33 @@ export const forgeContractAddress = '';
 export const forgeContractABI = [
   {
     "inputs": [
-        {
-            "internalType": "address",
-            "name": "tokensContract",
-            "type": "address"
-        }
+      {
+        "internalType": "address",
+        "name": "tokensContract",
+        "type": "address"
+      }
     ],
     "stateMutability": "nonpayable",
     "type": "constructor"
   },
   {
     "inputs": [
-        {
-            "internalType": "uint256",
-            "name": "tokenId",
-            "type": "uint256"
-        }
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
     ],
     "name": "NonForgeableTokenId",
     "type": "error"
   },
   {
     "inputs": [
-        {
-            "internalType": "uint256",
-            "name": "tokenId",
-            "type": "uint256"
-        }
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
     ],
     "name": "forge",
     "outputs": [],
@@ -40,11 +40,11 @@ export const forgeContractABI = [
     "inputs": [],
     "name": "tokenContract",
     "outputs": [
-        {
-            "internalType": "contract IMyERC1155Token",
-            "name": "",
-            "type": "address"
-        }
+      {
+        "internalType": "contract IMyERC1155Token",
+        "name": "",
+        "type": "address"
+      }
     ],
     "stateMutability": "view",
     "type": "function"
