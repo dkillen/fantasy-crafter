d88be0650a835920ce6c97c69311aa7b318ccceb
David Killen <39182959+dkillen@users.noreply.github.com>
2024-10-14 16:03:36 +1100

    Add contract abi and token metadata and images

--- app/utils/forgeContract.ts
+++ app/utils/forgeContract.ts
@@ -1,5 +1,52 @@
 export const forgeContractAddress = '';
 
 export const forgeContractABI = [
-  
+  {
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "tokensContract",
+            "type": "address"
+        }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+        {
+            "internalType": "uint256",
+            "name": "tokenId",
+            "type": "uint256"
+        }
+    ],
+    "name": "NonForgeableTokenId",
+    "type": "error"
+  },
+  {
+    "inputs": [
+        {
+            "internalType": "uint256",
+            "name": "tokenId",
+            "type": "uint256"
+        }
+    ],
+    "name": "forge",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "tokenContract",
+    "outputs": [
+        {
+            "internalType": "contract IMyERC1155Token",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
 ];
